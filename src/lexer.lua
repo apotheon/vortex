439f4724c5f53ebb26ac5465902a1308a0e975bc
quaker66 <quaker66@gmail.com>
2013-01-11 23:10:54 +0100

    A new lexer based around coroutines, no stacks needed anymore as well as that the token value doesn't have to be handled individually anymore

--- src/lexer.lua
+++ src/lexer.lua
@@ -19,6 +19,8 @@ local fatal        = util.fatal
 local concat       = table.concat
 local Stack        = util.Stack
 
+local wrap, yield = coroutine.wrap, coroutine.yield
+
 -- all of the core vortex keywords
 local keywords = {
     ["and"    ] = true,
@@ -113,19 +115,18 @@ end
 
 local lex
 
-local read_number = function(ls, token)
-    local buf = { ls.current }
+local read_number = function(ls, float)
+    local buf = float and { ".", ls.current } or { ls.current }
     next_char(ls)
     while ls.current and (is_alnum(ls.current) or ls.current == ".") do
         save_and_next_char(ls, buf)
     end
 
-    local str = table.concat(buf)
+    local str = concat(buf)
     if not tonumber(str) then
         lex_error(ls, "malformed number", str)
     end
-
-    token.value = str
+    yield("<number>", str)
 end
 
 local read_long_comment
@@ -195,32 +196,41 @@ local read_dec_esc = function(ls)
     return char(n)
 end
 
-local cw, cy = coroutine.wrap, coroutine.yield
-local readstr = function(ls, prefixes, delim, long)
-    cy()
-    local buf, sp = {}, prefixes or {}
-    local raw, expand = sp.raw, sp.expand
-
-    cy("<begstring>")
+local read_string = function(ls, pre)
+    local delim, long = ls.current
+    if next_char(ls) == delim then
+        if next_char(ls) == delim then
+            next_char(ls)
+            long = true
+        -- empty strings
+        else
+            yield("<begstring>")
+            yield("<string>", "")
+            yield("<endstring>")
+            return nil
+        end
+    end
+    pre = pre or {}
+    local raw, exp = pre.raw, pre.exp
+    local buf = {}
+    yield("<begstring>")
     while true do
         local curr = ls.current
         if not long then
             if curr == delim then
                 break
             end
-        else
+        elseif curr == delim then
+            curr = next_char(ls)
             if curr == delim then
                 curr = next_char(ls)
                 if curr == delim then
-                    curr = next_char(ls)
-                    if curr == delim then
-                        break
-                    else
-                        buf[#buf + 1] = delim .. delim
-                    end
+                    break
                 else
-                    buf[#buf + 1] = delim
+                    buf[#buf + 1] = delim .. delim
                 end
+            else
+                buf[#buf + 1] = delim
             end
         end
 
@@ -255,7 +265,6 @@ local readstr = function(ls, prefixes, delim, long)
                 buf[#buf + 1] = raw and "\\x" or read_hex_esc(ls, raw)
                 next_char(ls)
             elseif curr == "z" then
-                next_char(ls)
                 while is_newline(ls.current) or is_white(ls.current) do
                     if is_newline(ls.current) then
                         next_line(ls)
@@ -263,7 +272,7 @@ local readstr = function(ls, prefixes, delim, long)
                         next_char(ls)
                     end
                 end
-            elseif curr == "$" and expand then
+            elseif curr == "$" and exp then
                 buf[#buf + 1] = raw and "\\$" or "$"
                 next_char(ls)
             elseif curr == "\n" or curr == "\r" then
@@ -284,187 +293,108 @@ local readstr = function(ls, prefixes, delim, long)
                 end
                 buf[#buf + 1] = read_dec_esc(ls)
             end
-        elseif curr == "$" and expand then
+        elseif curr == "$" and exp then
             local str = concat(buf)
             buf = {}
-            cy("<string>", str)
+            yield("<string>", str)
             local c = next_char(ls)
-            local tok = {}
             if c == "(" then
-                cy("$(", nil)
+                yield("$(")
                 next_char(ls)
-                local level = 0
-                repeat
-                    local name = lex(ls, tok, true)
-                    if name == "(" then
-                        level = level + 1
-                    elseif name == ")" then
-                        level = level - 1
-                    end
-                    cy(name, tok.value)
-                until ls.current == ")" and level == 0
-                cy(")", nil)
-                next_char(ls)
-            else
-                cy("$", nil)
-                local name = lex(ls, tok, true)
-                if name == "<ident>" then
-                    cy(name, tok.value)
-                else
-                    buf[#buf + 1] = "$" .. (tok.value or name)
+                if not lex(ls, true) then
+                    lex_error(ls, "unfinished string", "<eos>")
                 end
+            elseif is_ident(c) and not is_digit(c) then
+                yield("$")
+                local id = { c }
+                next_char(ls)
+                while ls.current and is_ident(ls.current) do
+                    save_and_next_char(ls, id)
+                end
+                yield("<ident>", concat(id))
+            else
+                lex_error(ls, "expected identifier", curr)
             end
         else
             save_and_next_char(ls, buf)
         end
     end
     next_char(ls)
-    cy("<string>", concat(buf))
-    return "<endstring>"
+    yield("<string>", concat(buf))
+    yield("<endstring>")
 end
 
-local read_string = function(ls, prefixes)
-    local delim, long = ls.current
-    if next_char(ls) == delim then
-        if next_char(ls) == delim then
-            next_char(ls)
-            long = true
-        else
-            return ""
-        end
-    end
-    local coro = cw(readstr)
-    coro(ls, prefixes, delim, long)
-    return coro
-end
-
-local str_readers = Stack()
-lex = function(ls, token, instr)
-    token.value = nil
+lex = function(ls, instr)
+    local lvl = 0
     while true do
-        if not instr then
-            local rdr = str_readers:top()
-            if rdr then
-                local tok, val = rdr()
-                -- terminating token
-                if tok == "<endstring>" then
-                    str_readers:pop()
-                end
-                token.value = val
-                return tok
-            end
-        end
         local curr = ls.current
-
-        -- end of stream
         if not curr then
-            return "<eos>"
+            break
+        elseif instr and lvl == 0 and curr == ")" then
+            next_char(ls)
+            yield(")")
+            return true
         end
 
-        -- line breaks
         if is_newline(curr) then
             next_line(ls)
-        -- spaces
         elseif is_white(curr) then
             next_char(ls)
-        -- comments
-        -- strings, long strings
-
-        -- = or ==
-        elseif curr == "=" then
+        -- (, balancing checks - when delim is )
+        elseif curr == "(" then
             next_char(ls)
-            if ls.current ~= "=" then return "=" end
+            lvl = lvl + 1
+            yield("(")
+        -- )
+        elseif curr == ")" then
             next_char(ls)
-            return "=="
-        -- > or >=
-        elseif curr == ">" then
+            lvl = lvl - 1
+            yield(")")
+        -- =, ==, >, >=, <, <=, !, !=, %, %=
+        elseif curr == "=" or curr == ">" or curr == "<" or curr == "!"
+        or curr == "%" then
             next_char(ls)
-            if ls.current ~= "=" then return ">" end
+            if ls.current == "=" then
+                next_char(ls)
+                yield(curr .. "=")
+            else
+                yield(curr)
+            end
+        -- +, +=, ++, ++=, *, *=, **, **=
+        elseif curr == "+" or curr == "*" then
             next_char(ls)
-            return ">="
-        -- < or <=
-        elseif curr == "<" then
-            next_char(ls)
-            if ls.current ~= "=" then return "<" end
-            next_char(ls)
-            return "<="
-        -- ! or !=
-        elseif curr == "!" then
-            next_char(ls)
-            if ls.current ~= "=" then return "!" end
-            next_char(ls)
-            return "!="
-
-        -- +, +=, ++, ++=
-        elseif curr == "+" then
-            next_char(ls)
-
-            -- ++, ++=
-            if ls.current == "+" then
+            if ls.current == curr then
                 next_char(ls)
                 if ls.current == "=" then
                     next_char(ls)
-                    return "++="
+                    yield(curr .. curr .. "=")
+                else
+                    yield(curr .. curr)
                 end
-                return "++"
-            -- +=
             elseif ls.current == "=" then
                 next_char(ls)
-                return "+="
-            -- +
+                yield(curr .. "=")
             else
-                return "+"
+                yield(curr)
             end
         -- -, -=, ->
         elseif curr == "-" then
             next_char(ls)
-
-            -- ->
             if ls.current == ">" then
                 next_char(ls)
-                return "->"
-            -- -=
+                yield("->")
             elseif ls.current == "=" then
                 next_char(ls)
-                return "-="
-            -- -
+                yield("-=")
             else
-                return "-"
+                yield("-")
             end
-        -- *, *=, **, **=
-        elseif curr == "*" then
-            next_char(ls)
-
-            -- **, **=
-            if ls.current == "*" then
-                next_char(ls)
-                if ls.current == "=" then
-                    next_char(ls)
-                    return "**="
-                end
-                return "**"
-            -- *=
-            elseif ls.current == "=" then
-                next_char(ls)
-                return "*="
-            -- *
-            else
-                return "*"
-            end
-        -- %, %=
-        elseif curr == "%" then
-            next_char(ls)
-            if ls.current ~= "=" then return "%" end
-            next_char(ls)
-            return "%="
-
         -- /, /=, // (short comments), /* */ (long comments)
         elseif curr == "/" then
             next_char(ls)
-
             -- short comments
             if ls.current == "/" then
-                -- skip until EOL, EOS
+                -- skip until eol/eos
                 while ls.current and not is_newline(ls.current) do
                     next_char(ls)
                 end
@@ -475,134 +405,123 @@ lex = function(ls, token, instr)
             -- /=
             elseif ls.current == "=" then
                 next_char(ls)
-                return "/="
+                yield("/=")
             -- /
             else
-                return "/"
+                yield("/")
             end
-
         -- ., .., ..., numbers
         elseif curr == "." then
-            local buf = { curr }
             next_char(ls)
+            -- ... or ..
             if ls.current == "." then
                 next_char(ls)
+                -- ...
                 if ls.current == "." then
                     next_char(ls)
-                    return "..."
+                    yield("...")
+                -- ..
+                else
+                    yield("..")
                 end
-                return ".."
+            -- .
             elseif not is_digit(ls.current) then
-                return "."
+                yield(".")
+            -- .num
             else
-                read_number(ls, token)
-                return "<number>"
+                read_number(ls, true)
             end
-
         -- numbers
         elseif is_digit(curr) then
-            read_number(ls, token)
-            return "<number>"
-
+            read_number(ls)
         -- : or ::
         elseif curr == ":" then
             next_char(ls)
-            if ls.current ~= ":" then return ":" end
-            next_char(ls)
-            return "::"
-
+            if ls.current == ":" then
+                next_char(ls)
+                yield("::")
+            else
+                yield(":")
+            end
         -- $ or $(
         elseif curr == "$" then
             next_char(ls)
-            if ls.current ~= "(" then
+            if ls.current == "(" then
+                next_char(ls)
+                yield("$(")
+            else
                 if not is_ident(ls.current) then
                     lex_error(ls, "expected identifier", "$")
                 end
-                return "$"
+                yield("$")
             end
-            next_char(ls)
-            return "$("
-
         -- strings
         elseif curr == '"' or curr == "'" then
-            local r = read_string(ls)
-            if r == "" then
-                r = cw(function()
-                    cy("<string>", nil)
-                    cy("<string>", "")
-                    return "<string>", {}
-                end)
+            read_string(ls)
+        -- keywords, identifiers
+        elseif is_ident(curr) then
+            local buf = { curr }
+            next_char(ls)
+
+            local raw, exp = 0, 0
+            if curr == "e" or curr == "E" then
+                exp = exp + 1
+            elseif curr == "r" or curr == "R" then
+                raw = raw + 1
             end
-            local rdr = str_readers:push(r)
-            -- the "start" token
-            return rdr()
+            local strp = raw ~= 0 or exp ~= 0
 
-        -- keywords, identifiers, single-char tokens
-        else
-            -- keyword or identifier
-            if is_ident(curr) then
-                local buf = { curr }
-                next_char(ls)
-
-                local raw, expand = 0, 0
-                if curr == "e" or curr == "E" then
-                    expand = expand + 1
-                elseif curr == "r" or curr == "R" then
-                    raw = raw + 1
-                end
-                local strp = raw ~= 0 or expand ~= 0
-
-                while ls.current and is_ident(ls.current) do
-                    if strp then
-                        local curr = ls.current
-                        if curr == "e" or curr == "E" then
-                            expand = expand + 1
-                        elseif curr == "r" or curr == "R" then
-                            raw = raw + 1
-                        else
-                            raw, expand, strp = nil, nil, false
-                        end
+            while ls.current and is_ident(ls.current) do
+                if strp then
+                    local curr = ls.current
+                    if curr == "e" or curr == "E" then
+                        exp = exp + 1
+                    elseif curr == "r" or curr == "R" then
+                        raw = raw + 1
+                    else
+                        raw, exp, strp = nil, nil, false
                     end
-                    save_and_next_char(ls, buf)
                 end
+                save_and_next_char(ls, buf)
+            end
 
-                local str = table.concat(buf)
-                local kwop = kwops[str]
-                -- keyworded operator
-                if kwop == true or kwop == false then
-                    -- binary operator, assume compound
-                    if kwop == true and ls.current == "=" then
-                        next_char(ls)
-                        return str .. "="
-                    end
-                    return str
-                elseif keywords[str] then
-                    return str
+            local str = concat(buf)
+            local kwop = kwops[str]
+            -- a keyworded op
+            if kwop == true or kwop == false then
+                -- true kwops allow compound variants
+                if kwop == true and ls.current == "=" then
+                    next_char(ls)
+                    yield(str .. "=")
                 else
-                    if strp then
-                        local curr = ls.current
-                        if curr == '"' or curr == "'" then
-                            if raw > 1 or expand > 1 then
-                                lex_error(ls, "invalid string prefix", str)
-                            end
-
-                            local rdr = str_readers:push(read_string(ls, {
-                                raw = raw ~= 0, expand = expand ~= 0
-                            }))
-                            -- the "start" token
-                            return rdr()
-                        end
-                    end
-                    token.value = str
-                    return "<ident>"
+                    yield(str)
                 end
-            -- other single char tokens
+            -- a keyword
+            elseif keywords[str] then
+                yield(str)
+            -- a potential string
+            elseif strp then
+                local curr = ls.current
+                if curr == '"' or curr == "'" then
+                    if raw > 1 or exp > 1 then
+                        lex_error(ls, "invalid string prefix", str)
+                    end
+                    read_string(ls, { raw = raw ~= 0, exp = exp ~= 0 })
+                -- an identifier
+                else
+                    yield("<ident>", str)
+                end
+            -- an identifier
             else
-                next_char(ls)
-                return curr
+                yield("<ident>", str)
             end
+        -- any other singlechar token
+        else
+            next_char(ls)
+            yield(curr)
         end
     end
+    yield("<eos>")
 end
 
 local State_MT = {
@@ -619,7 +538,7 @@ local State_MT = {
                 tok.name, tok.value = lah.name, lah.value
                 lah.name, lah.value = nil, nil
             else
-                tok.name = lex(self, tok)
+                tok.name, tok.value = self:lex()
             end
 
             return tok.name
@@ -628,7 +547,7 @@ local State_MT = {
         -- retrieves a lookahead token when required
         lookahead = function(self)
             local lah = self.ltoken
-            lah.name = lex(self, lah)
+            lah.name, lah.value = self:lex()
             return lah.name
         end
     }
@@ -675,7 +594,8 @@ return {
             source      = fname,    -- the source (a filename or stdin or w/e)
             current     = skip_shebang(reader), -- the current character
             line_number = 1,        -- the current line number
-            last_line   = 1         -- previous line number
+            last_line   = 1,        -- previous line number
+            lex         = wrap(lex) -- the lexer coroutine used by this state
         }, State_MT)
     end,
 
